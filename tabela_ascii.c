a5758be9fc95ec3c6bb932d4f00d98a0740fcca9
clarissalima <clarissa.lima65@gmail.com>
2023-03-22 18:45:13 -0300

    exercicio simples que mostra a tebela ascii completa

---
+++ tabela_ascii.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+   {
+      int i, j, k;
+       char c;
+   
+      printf("Tabela ASCII completa\n\n");
+   
+      for (i = 0; i < 26; i++)
+      {
+          for (j = 0; j < 10; j++)
+          {
+              k = i * 10 + j;
+              printf("%3d %c  ", k, k);
+          }
+         putchar('\n');
+      }
+  
+      printf("\nDigite:Clarissa!\n", 135, 198);
+      do
+          putchar(c = getchar());
+      while (c != '\n');
+  
+      return(0);
+  }
